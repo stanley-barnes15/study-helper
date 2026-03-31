d683ba2186dd6ca960514bc0ce2ee47260bdb1cc
Stanley Barnes <stanleybarnes@MacBookAir-2.lan>
2026-03-31 21:32:53 +0100

    html unescaped JSON api inputs

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 import requests
+import html
 def get_question():
     url = 'https://opentdb.com/api.php?amount=1'
     response = requests.get(url)
     data = response.json()
     question = data['results'][0]['question']
+    question = html.unescape(question)
     print(f'{question}')
-    answer = input('Your answer:\n')
     correct_answer = data['results'][0]['correct_answer']
+    correct_answer = html.unescape(correct_answer)
     return question, correct_answer
