7ff998d9e523f9698e87b31c5e3664fb39c33969
Stanley Barnes <stanleybarnes@MacBookAir-2.lan>
2026-03-31 21:49:30 +0100

    changed the output for true or false

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ def get_question():
     data = response.json()
     question = data['results'][0]['question']
     question = html.unescape(question)
-    print(f'{question}')
+    print(f'True or False, {question}?')
     correct_answer = data['results'][0]['correct_answer']
     correct_answer = html.unescape(correct_answer)
     return question, correct_answer
