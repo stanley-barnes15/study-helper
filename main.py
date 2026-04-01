1285956a31abb580ef26d45bc6b6e9e3585bbbfc
Stanley Barnes <stanleybarnes@MacBookAir-2.lan>
2026-04-01 06:11:53 +0100

    now asks 10 questions instead of just one

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from api import get_question, get_user_category
-question, correct_answer = get_question(get_user_category())
-answer = input('Your answer:\n')
-if answer.lower() == correct_answer.lower():
-    print('Correct!')
-else:
-    print(f'Wrong! The correct answer is {correct_answer.lower()}.')
+questions, correct_answers = get_question(get_user_category())
+for question, correct_answer in zip(questions, correct_answers):
+    print(question)
+    answer = input('Your answer: ')
+    if answer.lower() == correct_answer.lower():
+        print('Correct!')
+    else:
+        print(f'Wrong! The correct answer is: {correct_answer}')
